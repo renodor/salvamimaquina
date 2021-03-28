469fec849b7fa456485942d40f1874a6a471d06c
renaud dor <renaud.dor@gmail.com>
2021-03-28 15:12:12 -0500

    Change attr id name on RepairShoprApi::V1::SyncProduct

--- app/controllers/repair_shopr_webhook/products_controller.rb
+++ app/controllers/repair_shopr_webhook/products_controller.rb
@@ -4,7 +4,7 @@ class RepairShoprWebhook::ProductsController < ApplicationController
   def product_updated
     return unless params['link'].include?("https://#{Rails.application.credentials.repair_shopr_subdomain_test}.repairshopr.com/products/")
 
-    RepairShoprApi::V1::SyncProduct.call(id: params['attributes']['id'])
+    RepairShoprApi::V1::SyncProduct.call(repair_shopr_id: params['attributes']['id'])
     head :ok
   end
 end
