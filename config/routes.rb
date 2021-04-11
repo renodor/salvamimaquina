463f39a9aeaecd9394198be4646fc7901370f080
renaud dor <renaud.dor@gmail.com>
2021-04-11 14:22:55 -0500

    Compile assets on prod deploy

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,6 @@ Rails.application.routes.draw do
     end
   end
 
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :repair_shopr_webhook do
