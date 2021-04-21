1a58634be2cfb946de4d4c6a2fa032c67fe2f930
renaud dor <renaud.dor@gmail.com>
2021-04-21 00:48:38 -0500

    Add shipping price and location id to RS invoices

--- app/services/repair_shopr_api/v1/create_invoice.rb
+++ app/services/repair_shopr_api/v1/create_invoice.rb
@@ -34,7 +34,7 @@ class RepairShoprApi::V1::CreateInvoice < RepairShoprApi::V1::Base
         total: order.total,
         tax: order.additional_tax_total,
         is_paid: order.payment_state == 'paid',
-        # location_id: TBD ??
+        location_id: define_invoice_location_id(order.shipments),
         line_items: []
       }
 
@@ -42,7 +42,7 @@ class RepairShoprApi::V1::CreateInvoice < RepairShoprApi::V1::Base
         repair_shopr_invoice[:line_items] << {
           item: line_item.variant.name,
           name: line_item.variant.description,
-          product_id: line_item.product.repair_shopr_id,
+          product_id: line_item.variant.repair_shopr_id,
           quantity: line_item.quantity,
           price: line_item.price * 1.07,
           taxable: true,
@@ -52,8 +52,33 @@ class RepairShoprApi::V1::CreateInvoice < RepairShoprApi::V1::Base
         }
       end
 
-      invoice = post_invoices(repair_shopr_invoice)
+      repair_shopr_invoice[:line_items] << {
+        item: 'Shipping',
+        name: 'Shipping Cost',
+        quantity: 1,
+        taxable: false,
+        price: order.shipment_total
+      }
+
+      post_invoices(repair_shopr_invoice)
       # Todo: send notif/error/email/anything... if invoice is not correctly created on RS...
+    end
+
+    # Bella Vista id = 1928
+    # San Francisco id = 1927
+    # Check the quantity of items by location
+    # Set the invoice location to the location that has more items
+    # If both locations have the same item quantities, set the invoice location to Bella Vista
+    def define_invoice_location_id(shipments)
+      stock_location_repair_shopr_ids = Hash.new(0)
+      shipments.each do |shipment|
+        shipment.line_items.map(&:quantity).sum
+        stock_location_repair_shopr_ids[shipment.stock_location.repair_shopr_id] += shipment.line_items.map(&:quantity).sum
+      end
+
+      return 1928 if stock_location_repair_shopr_ids[1928] == stock_location_repair_shopr_ids[1927]
+
+      stock_location_repair_shopr_ids.max_by { |_, qty| qty }[0]
     end
   end
 end
