8235650420bcdeb11c33f21f53eeba41f82162a9
renaud dor <renaud.dor@gmail.com>
2021-04-30 16:09:41 -0500

    Correct include method typo

--- app/controllers/repair_shopr_webhook/products_controller.rb
+++ app/controllers/repair_shopr_webhook/products_controller.rb
@@ -5,10 +5,8 @@ class RepairShoprWebhook::ProductsController < ApplicationController
     # Only accept requests coming from the correct RS sender
     return unless params['link'].include?("https://#{Rails.application.credentials.repair_shopr_subdomain}.repairshopr.com/products/")
 
-    # Only sync products belonging to the "ecom" category
-    return unless params['attributes']['product_categories'].includes?(RepairShoprApi::V1::Base::RS_ROOT_CATEGORY_NAME)
-    # Only sync enabled products
-    return if params['attributes']['disabled']
+    # Only sync products belonging to the "ecom" category and enabled products
+    return if !params['attributes']['product_category'].include?(RepairShoprApi::V1::Base::RS_ROOT_CATEGORY_NAME) || params['attributes']['disabled']
 
     # Don't sync the params['attributes'] directly as anyone could create such request
     # Instead just get the product id and fetch attributes of this product directly on RepairShopr
