554c1bd40de768cee50038fe04b1635f62e2cd21
renaud dor <renaud.dor@gmail.com>
2021-04-30 16:23:44 -0500

    Allow webhook controller to destroy variants

--- app/controllers/repair_shopr_webhook/products_controller.rb
+++ app/controllers/repair_shopr_webhook/products_controller.rb
@@ -5,12 +5,17 @@ class RepairShoprWebhook::ProductsController < ApplicationController
     # Only accept requests coming from the correct RS sender
     return unless params['link'].include?("https://#{Rails.application.credentials.repair_shopr_subdomain}.repairshopr.com/products/")
 
-    # Only sync products belonging to the "ecom" category and enabled products
-    return if !params['attributes']['product_category'].include?(RepairShoprApi::V1::Base::RS_ROOT_CATEGORY_NAME) || params['attributes']['disabled']
+    # Only sync products enabled and belonging to the "ecom" category
+    # If it is not the case but the variant exists, we need to destroy it
+    if params['attributes']['product_category'].include?(RepairShoprApi::V1::Base::RS_ROOT_CATEGORY_NAME) && !params['attributes']['disabled']
+      # Don't sync the params['attributes'] directly as anyone could create such request
+      # Instead just get the product id and fetch attributes of this product directly on RepairShopr
+      RepairShoprApi::V1::SyncProduct.call(sync_logs: RepairShoprProductsSyncLog.new, repair_shopr_id: params['attributes']['id'])
+    elsif (variant = Spree::Variant.find(repair_shopr_id: params['attributes']['id']))
+      variant.destroy!
+      product.destroy! unless product.has_variants?
+    end
 
-    # Don't sync the params['attributes'] directly as anyone could create such request
-    # Instead just get the product id and fetch attributes of this product directly on RepairShopr
-    RepairShoprApi::V1::SyncProduct.call(sync_logs: RepairShoprProductsSyncLog.new, repair_shopr_id: params['attributes']['id'])
     head :ok
   end
 end
