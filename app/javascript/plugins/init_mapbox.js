f32ecfa1994f7760449dadf52662c7737c2c5320
renaud dor <renaud.dor@gmail.com>
2021-05-03 13:33:30 -0500

    Add mapbox

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,13 @@
+import mapboxgl from 'mapbox-gl'
+
+const initMapbox = () => {
+  mapboxgl.accessToken = 'pk.eyJ1Ijoic2FsdmFtaW1hcXVpbmEiLCJhIjoiY2tvOHI4aDJrMHZybzJ2cHdtNDNwdG1zdCJ9.ui0HPCIdJ0fTiZw3Y_0iBg';
+  const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v11', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+  });
+}
+
+export { initMapbox }
