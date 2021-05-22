aab82e2ecf77e32db9bd557fe76707e6760ef85d
renaud dor <renaud.dor@gmail.com>
2021-05-22 17:40:56 -0500

    Tokenize

--- app/services/payment_gateway/first_atlantic_commerce/tokenize.rb
+++ app/services/payment_gateway/first_atlantic_commerce/tokenize.rb
@@ -1,24 +1,33 @@
 # frozen_string_literal: true
 
 module PaymentGateway
-  class FirstAtlanticCommerce::Tokenize < FirstAtlanticCommerce::Base
-    class << self
-      def call(card_number:, customer_reference:, expiry_date:)
-        xml_response = tokenize(xml_template(card_number, customer_reference, expiry_date))
+  module FirstAtlanticCommerce
+    class Tokenize < FirstAtlanticCommerce::Base
+      def initialize(card_number, customer_reference, expiry_date)
+        @card_number = card_number
+        @customer_reference = customer_reference
+        @expiry_date = expiry_date
+        @token = nil
+        @success = false
+      end
+
+      def call
+        xml_response = tokenize(xml_template)
         xml_parsed_response = Nokogiri::XML(xml_response).remove_namespaces!
 
         if xml_parsed_response.xpath('//Success').text == 'true'
-          xml_parsed_response.xpath('//Token').text
+          @success = true
+          @token = xml_parsed_response.xpath('//Token').text
         else
-          'Token Error' # TODO: Handle token error
+          @success = false
         end
       end
 
-      def xml_template(card_number, customer_reference, expiry_date)
+      def xml_template
         "<TokenizeRequest xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://schemas.firstatlanticcommerce.com/gateway/data\">
-          <CardNumber>#{card_number}</CardNumber>
-          <CustomerReference>#{customer_reference}</CustomerReference>
-          <ExpiryDate>#{expiry_date}</ExpiryDate>
+          <CardNumber>#{@card_number}</CardNumber>
+          <CustomerReference>#{@customer_reference}</CustomerReference>
+          <ExpiryDate>#{@expiry_date}</ExpiryDate>
           <MerchantNumber>#{FirstAtlanticCommerce::Base::MERCHANT_ID}</MerchantNumber>
           <Signature>#{signature}</Signature>
         </TokenizeRequest>"
@@ -26,6 +35,10 @@ module PaymentGateway
 
       def signature
         Digest::SHA1.base64digest("#{FirstAtlanticCommerce::Base::PASSWORD}#{FirstAtlanticCommerce::Base::MERCHANT_ID}#{FirstAtlanticCommerce::Base::ACQUIRER_ID}")
+      end
+
+      def success?
+        @success
       end
     end
   end
