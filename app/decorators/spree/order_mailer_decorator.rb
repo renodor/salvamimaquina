e315fa8b42bf09def479215142edd3408a28e796
renaud dor <renaud.dor@gmail.com>
2021-09-02 09:34:44 +0200

    Finalize confirm email

--- app/decorators/spree/order_mailer_decorator.rb
+++ app/decorators/spree/order_mailer_decorator.rb
@@ -6,10 +6,10 @@ module Spree
       @order = order
       @address = @order.ship_address
       @store = @order.store
-      I18n.with_locale('es-MX') do # Currently forcing emails in Spanish (we don't want the locale to change regarding user browser's language)
-        subject = build_subject(t('.subject'), resend)
-        mail(to: @order.email, bcc: bcc_address(@store), from: from_address(@store), subject: subject)
-      end
+      # I18n.with_locale('es-MX') do # Currently forcing emails in Spanish (we don't want the locale to change regarding user browser's language)
+      subject = build_subject(t('.subject'), resend)
+      mail(to: @order.email, bcc: bcc_address(@store), from: from_address(@store), subject: subject)
+      # end
     end
 
     private
