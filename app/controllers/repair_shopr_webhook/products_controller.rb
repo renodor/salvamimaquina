636aaff5c23eda4916865f9b42019c74f7836130
Renaud Dor <renaud.dor@gmail.com>
2021-09-10 02:20:22 +0200

    Add safe operator on updated_product webhook for products without category

--- app/controllers/repair_shopr_webhook/products_controller.rb
+++ app/controllers/repair_shopr_webhook/products_controller.rb
@@ -13,7 +13,7 @@ class RepairShoprWebhook::ProductsController < ApplicationController
 
     # Only sync products enabled and belonging to the "ecom" category
     # If it is not the case but the variant exists, we need to destroy it
-    if params['attributes']['product_category'].include?(RepairShoprApi::V1::Base::RS_ROOT_CATEGORY_NAME) && !params['attributes']['disabled']
+    if params['attributes']['product_category']&.include?(RepairShoprApi::V1::Base::RS_ROOT_CATEGORY_NAME) && !params['attributes']['disabled']
       # Don't sync the params['attributes'] directly as anyone could create such request
       # Instead just get the product id and fetch attributes of this product directly on RepairShopr
       RepairShoprApi::V1::SyncProduct.call(sync_logs: RepairShoprProductsSyncLog.new, repair_shopr_id: params['attributes']['id'])
