d2eb3bc84a191a24684686daf347d7565dbc6357
renaud dor <renaud.dor@gmail.com>
2021-10-29 12:13:54 +0200

    Remove fake error in RepairShopr sync that was added for testing

--- app/services/repair_shopr_api/v1/sync.rb
+++ app/services/repair_shopr_api/v1/sync.rb
@@ -8,7 +8,6 @@ class RepairShoprApi::V1::Sync < RepairShoprApi::V1::Base
       taxon_ids = RepairShoprApi::V1::SyncProductCategories.call(sync_logs: sync_logs)
       RepairShoprApi::V1::SyncProducts.call(sync_logs: sync_logs) if taxon_ids
 
-      sync_logs.sync_errors << { fake_error: 'to test' }
       sync_logs.status = sync_logs.sync_errors.any? ? 'error' : 'complete'
       sync_logs.save!
       Sentry.capture_message(name, { extra: { sync_logs_errors: sync_logs.sync_errors } }) if sync_logs.sync_errors.any?
