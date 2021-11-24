bed24b4639a38c66ab6fc8cc6eecd16858c51277
renaud dor <renaud.dor@gmail.com>
2021-11-24 19:03:43 +0100

    Set action_mailer.raise_delivery_errors to false in PROD

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,17 +61,16 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :postmark
-  config.action_mailer.postmark_settings = {
-    api_token: Rails.application.credentials.postmark_api_token
-  }
+  config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.default_url_options = { host: 'https://www.salvamimaquina.com' }
+  config.action_mailer.postmark_settings   = { api_token: Rails.application.credentials.postmark_api_token }
 
   # Enable serving of images from an asset server for emails
   config.action_mailer.asset_host = 'https://www.salvamimaquina.com/'
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
