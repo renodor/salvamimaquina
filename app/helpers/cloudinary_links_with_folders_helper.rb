7c3ac9bce6971ced50a0bc85db9ce9840b1b54ca
renaud dor <renaud.dor@gmail.com>
2022-01-21 17:29:35 +0100

    Add comments to explain Cloudinary storage with folders feature and add hints with image expected dimensions to forms with images

--- app/helpers/cloudinary_links_with_folders_helper.rb
+++ app/helpers/cloudinary_links_with_folders_helper.rb
@@ -1,12 +1,31 @@
 # frozen_string_literal:true
 
+# Extend Cloudinary helper methods to use folders and fallback images.
+
+# Folders:
+# - A CLOUDINARY_STORAGE_FOLDER constant need to be defined on the model with the folder of the service
+# - when using #cl_image_path_with_folder or #cl_image_tag_with_folder, the model name need to be added to the options argument
+# For example:
+# For Slide model, We want to use the cloudinary_slides service. This service folder is 'slides',
+# so on the Slide model we defined CLOUDINARY_STORAGE_FOLDER = 'slides'
+# Then when displaying a slide image we call: cl_image_tag(slide_image, model: Slide)
+
+# Fallback images:
+# - A fallback image need to be uploaded to Cloudinary in the correct folder
+# - A CLOUDINARY_FALLBACK_IMAGE constant need to be defined on the model with the correct fallback image file name
+# For example:
+# For Slide model, we uploaded a fallback image with the name 'slide-placeholder.jpg' on the {{ Rails.env }}/slides folder,
+# so on the Slide model we defined CLOUDINARY_FALLBACK_IMAGE = 'slide-placeholder.jpg'
+# Now when displaying a slide image, if the file cannot be found, Cloudinary will automatically fallback to slide-placeholder.jpg image
+# (Except if no_fallback is added to the options argument. Ex: cl_image_tag_with_folder(slide_image, model: Slide, no_fallback: true) )
+
 module CloudinaryLinksWithFoldersHelper
   def cl_image_path_with_folder(image, options = {})
-    cl_image_path(image_url(image, options), options_with_folder(options))
+    cl_image_path(image_url(image, options), options_with_fallback_image(options))
   end
 
   def cl_image_tag_with_folder(image, options = {})
-    cl_image_tag(image_url(image, options), options_with_folder(options))
+    cl_image_tag(image_url(image, options), options_with_fallback_image(options))
   end
 
   private
@@ -19,7 +38,7 @@ module CloudinaryLinksWithFoldersHelper
     "#{Rails.env}/#{options[:model]::CLOUDINARY_STORAGE_FOLDER}/#{image.key}"
   end
 
-  def options_with_folder(options)
+  def options_with_fallback_image(options)
     return options unless options[:model].const_defined?('CLOUDINARY_FALLBACK_IMAGE')
 
     options[:default_image] = "#{Rails.env}:#{options[:model]::CLOUDINARY_STORAGE_FOLDER}:#{options[:model]::CLOUDINARY_FALLBACK_IMAGE}"
--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -1,10 +1,27 @@
 # frozen_string_literal:true
 
-model_names = %w[products product_categories slides banners reparation_categories]
+# Dinamically create storage service for the following folders.
+# (Then we need to choose what model will have an attachment using those services)
+
+# For example, adding "slides" in the folder list here will create the following service:
+# cloudinary_slides: {
+#   service: 'Cloudinary',
+#   folder: '{{ Rails.env }}/slides'
+# }
+# So on the Slide model we can now define:
+# has_one_attached :image, service: :cloudinary_slides
+# Now each time an attachment is create for Slide model, the image will be automatically uploaded in Cloudinary in the following folder:
+# - /development/slides (in dev environment)
+# - /production/slides (in prod environment)
+
+# (Then when displaying images on the front end don't forget to use #cl_image_path_with_folder and #cl_image_tag_with_folder methods
+# to retrieve the files on the correct folder)
+
+folders = %w[products product_categories slides banners reparation_categories]
 
 service_configuration = {}
-model_names.each do |model_name|
-  service_configuration["cloudinary_#{model_name}"] = { 'service' => 'Cloudinary', 'folder' => "#{Rails.env}/#{model_name}" }
+folders.each do |folder|
+  service_configuration["cloudinary_#{folder}"] = { 'service' => 'Cloudinary', 'folder' => "#{Rails.env}/#{folder}" }
 end
 
 Rails.configuration.active_storage.service_configurations = service_configuration
