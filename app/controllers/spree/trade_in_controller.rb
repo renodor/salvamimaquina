8bb7e03181a958f06200785a5dba5639a2f2ab01
renaud dor <renaud.dor@gmail.com>
2022-01-25 21:59:55 +0100

    Work on trade in form

--- app/controllers/spree/trade_in_controller.rb
+++ app/controllers/spree/trade_in_controller.rb
@@ -4,17 +4,10 @@ module Spree
   class TradeInController < Spree::StoreController
     def index
       @trade_in_categories = TradeInCategory.all
-      @trade_in_categories_options = @trade_in_categories.map { |category| [category.name, category.id] }
-
-      # @trade_in_models_options = @trade_in_categories.map do |category|
-      #   {
-      #     category_id: category.id,
-      #     model_options: category.trade_in_models.map { |model| [model.name, model.id] },
-      #     model_prices: category.trade_in_models.map { |model| [model.min_value, model.max_value] }
-      #   }
-      # end
-
       @trade_in_models = TradeInModel.all
+      @taxons = Taxon.where(depth: 1)
+      @products = Spree::Product.all
+      @variants = Spree::Variant.all
     end
 
     def show
--- app/javascript/components/trade_in_form.js
+++ app/javascript/components/trade_in_form.js
@@ -19,6 +19,21 @@ const tradeInForm = () => {
     });
 
     displaySelectedTradeInModelPrices(selectCategories.value);
+
+
+    const selectModels = tradeInForm.querySelectorAll('#trade-in-models select');
+    selectModels.forEach((selectModel) => {
+      selectModel.addEventListener('change', (event) => {
+        const selectedModelId = event.currentTarget.value;
+        tradeInForm.querySelectorAll('#trade-in-model-prices .trade-in-model-price').forEach((tradeInModelPrice) => {
+          if (tradeInModelPrice.dataset.modelId == selectedModelId) {
+            tradeInModelPrice.classList.remove('display-none');
+          } else {
+            tradeInModelPrice.classList.add('display-none');
+          }
+        });
+      });
+    });
   }
 };
 
