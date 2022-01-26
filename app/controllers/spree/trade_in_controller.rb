eddb1f1e25da26435911b74872a122740ee9990f
renaud dor <renaud.dor@gmail.com>
2022-01-26 23:05:28 +0100

    Add JS hide/reveal methods and try to reveal blocks with CSS transitions

--- app/controllers/spree/trade_in_controller.rb
+++ app/controllers/spree/trade_in_controller.rb
@@ -3,11 +3,11 @@
 module Spree
   class TradeInController < Spree::StoreController
     def index
-      @trade_in_categories = TradeInCategory.all
+      @trade_in_categories = TradeInCategory.all.includes(:trade_in_models)
       @trade_in_models = TradeInModel.all
       @taxons = Taxon.where(depth: 1)
-      @products = Spree::Product.all
-      @variants = Spree::Variant.all
+      @products = Spree::Product.all.includes(:variants).order(:name)
+      @variants = Spree::Variant.all.includes(:product, [option_values: :option_type]).order(:product_id)
     end
 
     def show
--- app/javascript/components/trade_in_form.js
+++ app/javascript/components/trade_in_form.js
@@ -2,14 +2,38 @@ const tradeInForm = () => {
   const tradeInForm = document.getElementById('trade-in-form');
 
   if (tradeInForm) {
-    const selectCategories = tradeInForm.querySelector('#trade_in_categories');
+    const relvealElementWithTransition = (element) => {
+      element.classList.remove('display-none');
+      window.setTimeout(() => {
+        element.classList.remove('hidden');
+      }, 0);
+      element.dataset.hidden = false;
+    };
 
+    const relvealElement = (element) => {
+      element.classList.remove('display-none');
+      element.classList.remove('hidden');
+      element.dataset.hidden = false;
+    };
+
+    const hideElement = (element) => {
+      element.classList.add('display-none');
+      element.classList.add('hidden');
+      element.dataset.hidden = true;
+    };
+
+    const selectCategories = tradeInForm.querySelector('#trade_in_categories');
     const displaySelectedTradeInModelPrices = (selectedTradeInModelId) => {
-      tradeInForm.querySelectorAll('#trade-in-models select').forEach((tradeInModel) => {
-        if (tradeInModel.id == selectedTradeInModelId) {
-          tradeInModel.classList.remove('display-none');
+      const selectModels = tradeInForm.querySelectorAll('#trade-in-models select');
+      selectModels.forEach((tradeInModel) => {
+        if (tradeInModel.dataset.parentId == selectedTradeInModelId) {
+          if (Array.from(selectModels).every((model) => model.dataset.hidden == 'false')) {
+            relvealElement(tradeInModel);
+          } else {
+            relvealElementWithTransition(tradeInModel);
+          }
         } else {
-          tradeInModel.classList.add('display-none');
+          hideElement(tradeInModel);
         }
       });
     };
@@ -20,16 +44,68 @@ const tradeInForm = () => {
 
     displaySelectedTradeInModelPrices(selectCategories.value);
 
-
     const selectModels = tradeInForm.querySelectorAll('#trade-in-models select');
     selectModels.forEach((selectModel) => {
       selectModel.addEventListener('change', (event) => {
         const selectedModelId = event.currentTarget.value;
+        tradeInForm.querySelector('#trade-in-second-part').classList.remove('hidden');
         tradeInForm.querySelectorAll('#trade-in-model-prices .trade-in-model-price').forEach((tradeInModelPrice) => {
-          if (tradeInModelPrice.dataset.modelId == selectedModelId) {
-            tradeInModelPrice.classList.remove('display-none');
+          if (tradeInModelPrice.dataset.parentId == selectedModelId) {
+            tradeInModelPrice.classList.remove('hidden');
+            tradeInModelPrice.dataset.selected = true;
           } else {
-            tradeInModelPrice.classList.add('display-none');
+            tradeInModelPrice.classList.add('hidden');
+            tradeInModelPrice.dataset.selected = false;
+          }
+        });
+      });
+    });
+
+    const selectTaxon = tradeInForm.querySelector('#taxons select');
+    selectTaxon.addEventListener('change', (event) => {
+      const selectedTaxonId = event.currentTarget.value;
+      tradeInForm.querySelectorAll('#products .product').forEach((product) => {
+        if (product.dataset.parentId == selectedTaxonId) {
+          product.classList.remove('hidden');
+        } else {
+          product.classList.add('hidden');
+        }
+      });
+    });
+
+    const selectProducts = tradeInForm.querySelectorAll('#products select');
+    selectProducts.forEach((selectProduct) => {
+      selectProduct.addEventListener('change', (event) => {
+        const selectedProductId = event.currentTarget.value;
+        tradeInForm.querySelectorAll('#variants .variant').forEach((variant) => {
+          if (variant.dataset.parentId == selectedProductId) {
+            variant.classList.remove('hidden');
+          } else {
+            variant.classList.add('hidden');
+          }
+        });
+      });
+    });
+
+    const selectVariants = tradeInForm.querySelectorAll('#variants select');
+    selectVariants.forEach((selectVariant) => {
+      selectVariant.addEventListener('change', (event) => {
+        const selectedVariantId = event.currentTarget.value;
+        tradeInForm.querySelectorAll('#variant-infos .variant-info').forEach((variantInfo) => {
+          if (variantInfo.dataset.parentId == selectedVariantId) {
+            variantInfo.classList.remove('hidden');
+
+            const selectedTradeInModelPrice = tradeInForm.querySelector('#trade-in-model-prices .trade-in-model-price[data-selected=true]');
+            const minValue = parseFloat(selectedTradeInModelPrice.dataset.minValue);
+            const maxValue = parseFloat(selectedTradeInModelPrice.dataset.maxValue);
+
+            const variantPrice = parseFloat(variantInfo.dataset.price);
+            const priceToCompute = variantInfo.querySelector('.price-to-compute');
+
+            priceToCompute.querySelector('.min-price').innerHTML = variantPrice - minValue;
+            priceToCompute.querySelector('.max-price').innerHTML = variantPrice - maxValue;
+          } else {
+            variantInfo.classList.add('hidden');
           }
         });
       });
