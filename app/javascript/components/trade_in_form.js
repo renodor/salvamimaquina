2072c10aee2d6329c523b1a573c89c9515f16ed3
renaud dor <renaud.dor@gmail.com>
2022-01-27 17:11:48 +0100

    Start to refacto JS

--- app/javascript/components/trade_in_form.js
+++ app/javascript/components/trade_in_form.js
@@ -2,88 +2,48 @@ const tradeInForm = () => {
   const tradeInForm = document.getElementById('trade-in-form');
 
   if (tradeInForm) {
-    const relvealElementWithTransition = (element) => {
-      element.classList.remove('display-none');
-      window.setTimeout(() => {
-        element.classList.remove('hidden');
-      }, 0);
-      element.dataset.hidden = false;
-    };
-
-    const relvealElement = (element) => {
-      element.classList.remove('display-none');
-      element.classList.remove('hidden');
-      element.dataset.hidden = false;
-    };
-
-    const hideElement = (element) => {
-      element.classList.add('display-none');
-      element.classList.add('hidden');
-      element.dataset.hidden = true;
-    };
-
-    const selectCategories = tradeInForm.querySelector('#trade_in_categories');
-    const displaySelectedTradeInModelPrices = (selectedTradeInModelId) => {
-      const selectModels = tradeInForm.querySelectorAll('#trade-in-models select');
-      selectModels.forEach((tradeInModel) => {
-        if (tradeInModel.dataset.parentId == selectedTradeInModelId) {
-          if (Array.from(selectModels).every((model) => model.dataset.hidden == 'false')) {
-            relvealElement(tradeInModel);
-          } else {
-            relvealElementWithTransition(tradeInModel);
-          }
+    const displayCorrectChildren = (parentId, children) => {
+      children.forEach((childElement) => {
+        if (childElement.dataset.parentId === parentId) {
+          childElement.classList.remove('display-none');
+          childElement.dataset.selected = true;
         } else {
-          hideElement(tradeInModel);
+          childElement.classList.add('display-none');
+          childElement.dataset.selected = false;
         }
       });
     };
 
+    const selectCategories = tradeInForm.querySelector('#trade_in_categories');
     selectCategories.addEventListener('change', (event) => {
-      displaySelectedTradeInModelPrices(event.currentTarget.value);
+      const tradeInModels = tradeInForm.querySelector('#trade-in-models');
+      tradeInModels.classList.remove('hidden');
+      displayCorrectChildren(event.currentTarget.value, tradeInModels.querySelectorAll('.trade-in-model'));
     });
-
-    displaySelectedTradeInModelPrices(selectCategories.value);
 
     const selectModels = tradeInForm.querySelectorAll('#trade-in-models select');
     selectModels.forEach((selectModel) => {
       selectModel.addEventListener('change', (event) => {
-        const selectedModelId = event.currentTarget.value;
+        const tradeInModelPrices = tradeInForm.querySelector('#trade-in-model-prices');
+        tradeInModelPrices.classList.remove('hidden');
+        displayCorrectChildren(event.currentTarget.value, tradeInModelPrices.querySelectorAll('.trade-in-model-price'));
         tradeInForm.querySelector('#trade-in-second-part').classList.remove('hidden');
-        tradeInForm.querySelectorAll('#trade-in-model-prices .trade-in-model-price').forEach((tradeInModelPrice) => {
-          if (tradeInModelPrice.dataset.parentId == selectedModelId) {
-            tradeInModelPrice.classList.remove('hidden');
-            tradeInModelPrice.dataset.selected = true;
-          } else {
-            tradeInModelPrice.classList.add('hidden');
-            tradeInModelPrice.dataset.selected = false;
-          }
-        });
       });
     });
 
     const selectTaxon = tradeInForm.querySelector('#taxons select');
     selectTaxon.addEventListener('change', (event) => {
-      const selectedTaxonId = event.currentTarget.value;
-      tradeInForm.querySelectorAll('#products .product').forEach((product) => {
-        if (product.dataset.parentId == selectedTaxonId) {
-          product.classList.remove('hidden');
-        } else {
-          product.classList.add('hidden');
-        }
-      });
+      const products = tradeInForm.querySelector('#products');
+      products.classList.remove('hidden');
+      displayCorrectChildren(event.currentTarget.value, products.querySelectorAll('.product'));
     });
 
     const selectProducts = tradeInForm.querySelectorAll('#products select');
     selectProducts.forEach((selectProduct) => {
       selectProduct.addEventListener('change', (event) => {
-        const selectedProductId = event.currentTarget.value;
-        tradeInForm.querySelectorAll('#variants .variant').forEach((variant) => {
-          if (variant.dataset.parentId == selectedProductId) {
-            variant.classList.remove('hidden');
-          } else {
-            variant.classList.add('hidden');
-          }
-        });
+        const variants = tradeInForm.querySelector('#variants');
+        variants.classList.remove('hidden');
+        displayCorrectChildren(event.currentTarget.value, variants.querySelectorAll('.variant'));
       });
     });
 
@@ -93,7 +53,10 @@ const tradeInForm = () => {
         const selectedVariantId = event.currentTarget.value;
         tradeInForm.querySelectorAll('#variant-infos .variant-info').forEach((variantInfo) => {
           if (variantInfo.dataset.parentId == selectedVariantId) {
-            variantInfo.classList.remove('hidden');
+            variantInfo.classList.remove('display-none');
+            window.setTimeout(() => {
+              variantInfo.classList.remove('hidden');
+            }, 0);
 
             const selectedTradeInModelPrice = tradeInForm.querySelector('#trade-in-model-prices .trade-in-model-price[data-selected=true]');
             const minValue = parseFloat(selectedTradeInModelPrice.dataset.minValue);
@@ -105,7 +68,7 @@ const tradeInForm = () => {
             priceToCompute.querySelector('.min-price').innerHTML = variantPrice - minValue;
             priceToCompute.querySelector('.max-price').innerHTML = variantPrice - maxValue;
           } else {
-            variantInfo.classList.add('hidden');
+            variantInfo.classList.add('display-none', 'hidden');
           }
         });
       });
