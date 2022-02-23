ce3d4131355898acf559bd13a9da168f997f7f43
renaud dor <renaud.dor@gmail.com>
2022-02-23 11:00:26 +0100

    Change Action mailer default url options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'http://localhost:3000/' }
   config.action_mailer.delivery_method = :letter_opener
 
   # Print deprecation notices to the Rails logger.
