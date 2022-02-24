9b0277c66eb0073c5105311b18d4635ec93a52e9
renaud dor <renaud.dor@gmail.com>
2022-02-24 12:06:25 +0100

    Prevent scroll when menu mobile is open

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,11 +2,13 @@
 const navbar = () => {
   const navbarTogglers = document.querySelectorAll('.navbar-toggler');
   const navbarCollapse = document.querySelector('.navbar-collapse');
+  const body = document.querySelector('body');
 
   // listen clicks on the navbar toggle and open the navbar when clicked
   navbarTogglers.forEach((navbarToggler) => {
     navbarToggler.addEventListener('click', () => {
       navbarCollapse.classList.toggle('active');
+      body.classList.toggle('overflow-hidden');
     });
   });
 };
