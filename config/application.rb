8569055fa7c326056fe05b75056c54bda5427463
renaud dor <renaud.dor@gmail.com>
2022-03-03 11:55:07 +0100

    Add Rack::Deflater middleware

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,9 @@ module Salvamimaquina
 
     config.i18n.default_locale = 'es-MX'
 
+    # Compress HTML
+    config.middleware.use Rack::Deflater
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
