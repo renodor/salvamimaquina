b16dfcb324e62962926f56eb44ad68c08bec6347
renaud dor <renaud.dor@gmail.com>
2022-03-30 11:32:31 +0200

    Add more info on admin sliders index

--- app/controllers/spree/admin/sliders_controller.rb
+++ app/controllers/spree/admin/sliders_controller.rb
@@ -13,7 +13,6 @@ module Spree
 
       def edit
         @slider = Slider.find(params[:id])
-        @slider_location_url = location_url
       end
 
       def create
@@ -43,25 +42,6 @@ module Spree
 
       def slider_params
         params.require(:slider).permit(:location, :name, :auto_play, :navigation, :pagination, :fallback_image, :delay_between_slides, :space_between_slides, :force_slide_full_width, :image_per_slide_s, :image_per_slide_m, :image_per_slide_l, :image_per_slide_xl)
-      end
-
-      def location_url
-        case @slider.location
-        when 'home_page'
-          root_path(anchor: "#{@slider.location}-slider")
-        when 'products'
-          products_es_mx_path(anchor: "#{@slider.location}-slider")
-        when 'trade_in'
-          new_trade_in_request_path(anchor: "#{@slider.location}-slider")
-        when 'reparation'
-          reparation_categories_path(anchor: "#{@slider.location}-slider")
-        when 'corporate_clients_1', 'corporate_clients_2'
-          corporate_clients_path(anchor: "#{@slider.location}-slider")
-        when 'about'
-          about_path(anchor: "#{@slider.location}-slider")
-        when 'contact'
-          contact_path(anchor: "#{@slider.location}-slider")
-        end
       end
     end
   end
