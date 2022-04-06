99aca08cf70973cd5224a6a9cf15fdc481701525
renaud dor <renaud.dor@gmail.com>
2022-04-06 15:01:17 +0200

    Add comments and rename files

--- app/javascript/components/flash.js
+++ app/javascript/components/flash.js
@@ -1,5 +1,5 @@
 // Add the given flash (html tag as a string) to the DOM, and then add dynamism to it
-// This method is a bit special, instead of beign imported in application.js,
+// This method is a bit special, indeed instead of being imported in application.js,
 // it is imported in other modules when we need to add a flash after an ajax request (ex: add_to_cart.js)
 const addFlashToDom = (flash) => {
   const content = document.querySelector('body #wrapper #content');
@@ -8,13 +8,13 @@ const addFlashToDom = (flash) => {
 };
 
 // Remove flash when clicking on cross and auto remove it after 10 seconds
+// (is imported in application.js, so will be called automatically when page load,
+// and is also called by addFlashToDom() when needed)
 const addDinamysmToFlash = () => {
   const flash = document.querySelector('.flash');
 
   if (flash) {
-    flash.querySelector('svg.close-flash').addEventListener('click', () => {
-      flash.remove();
-    });
+    flash.querySelector('svg.close-flash').addEventListener('click', () => (flash.remove()));
 
     setTimeout(() => {
       flash.remove();
