081f16bbeec21ba9e0f1833c5d41acc46f13c864
renaud dor <renaud.dor@gmail.com>
2022-06-08 11:01:28 +0200

    Remove non numerical characters from address phone before validation

--- app/decorators/spree/address_decorator.rb
+++ app/decorators/spree/address_decorator.rb
@@ -4,6 +4,7 @@ module Spree
   module AddressDecorator
     def self.prepended(base)
       base.belongs_to :district, class_name: 'Spree::District', optional: true
+      base.before_validation :remove_non_numerical_charaters_from_phone
     end
 
     def require_zipcode?
@@ -12,6 +13,10 @@ module Spree
 
     def google_maps_link
       "http://www.google.com/maps/place/#{latitude},#{longitude}"
+    end
+
+    def remove_non_numerical_charaters_from_phone
+      phone.gsub!(/\D/, '')
     end
 
     Spree::Address.prepend self
