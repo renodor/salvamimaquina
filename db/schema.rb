ca59438a1f28016dd2047f8910469d0b3d478437
renaud dor <renaud.dor@gmail.com>
2022-10-03 17:37:18 +0200

    Add migration again

--- db/schema.rb
+++ db/schema.rb
@@ -481,6 +481,8 @@ ActiveRecord::Schema.define(version: 2022_10_03_145852) do
     t.string "number"
     t.string "cvv_response_code"
     t.string "cvv_response_message"
+    t.uuid "uuid"
+    t.string "spi_token"
     t.index ["number"], name: "index_spree_payments_on_number", unique: true
     t.index ["order_id"], name: "index_spree_payments_on_order_id"
     t.index ["payment_method_id"], name: "index_spree_payments_on_payment_method_id"
@@ -1387,18 +1389,6 @@ ActiveRecord::Schema.define(version: 2022_10_03_145852) do
     t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
