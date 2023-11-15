399d09f5f54152b6a5051870fc72f27e6aaecaef
renodor <renaud.dor@gmail.com>
2023-11-15 12:01:18 +0100

    Add first migration and change database

---
+++ db/migrate/20231115105837_add_repair_shopr_id_to_product.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddRepairShoprIdToProduct < ActiveRecord::Migration[6.0]
+  def change
+    add_column :spree_products, :repair_shopr_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_15_101918) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_15_105837) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -475,6 +475,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_15_101918) do
     t.boolean "promotionable", default: true
     t.string "meta_title"
     t.datetime "discontinue_on", precision: nil
+    t.integer "repair_shopr_id"
     t.index ["available_on"], name: "index_spree_products_on_available_on"
     t.index ["deleted_at"], name: "index_spree_products_on_deleted_at"
     t.index ["name"], name: "index_spree_products_on_name"
