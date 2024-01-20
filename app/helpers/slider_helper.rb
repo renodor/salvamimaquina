22738b16561ddac8660bb7710e1e08676aff2f96
renodor <renaud.dor@gmail.com>
2024-01-20 14:45:36 +0100

    Work on admin doc

--- app/helpers/slider_helper.rb
+++ app/helpers/slider_helper.rb
@@ -5,19 +5,19 @@ module SliderHelper
     slider_css_id = "#{slider.location}-slider"
     case slider.location
     when 'home_page'
-      root_path(anchor: slider_css_id)
+      main_app.root_path(anchor: slider_css_id)
     when 'products'
-      products_es_mx_path(anchor: slider_css_id)
+      main_app.products_es_mx_path(anchor: slider_css_id)
     when 'trade_in'
-      new_trade_in_request_path(anchor: slider_css_id)
+      main_app.new_trade_in_request_path(anchor: slider_css_id)
     when 'reparation'
-      reparation_categories_path(anchor: slider_css_id)
+      main_app.reparation_categories_path(anchor: slider_css_id)
     when 'corporate_clients_1', 'corporate_clients_2'
-      corporate_clients_path(anchor: slider_css_id)
+      main_app.corporate_clients_path(anchor: slider_css_id)
     when 'about'
-      about_path(anchor: slider_css_id)
+      main_app.about_path(anchor: slider_css_id)
     when 'contact'
-      contact_path(anchor: slider_css_id)
+      main_app.contact_path(anchor: slider_css_id)
     end
   end
 end
--- app/helpers/taxons_helper.rb
+++ app/helpers/taxons_helper.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module TaxonsHelper
   def taxon_seo_url(taxon)
     nested_taxons_path(taxon.permalink)
