398462cf909ec45e4e6beb3b731a267c4703a7db
renodor <renaud.dor@gmail.com>
2024-04-25 11:05:50 +0200

    Remove redis initializaition from Sidekiq initializer

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-# rubocop:disable Style/GlobalVars
-$redis = Redis.new
-
 url = ENV['REDISCLOUD_URL']
 
 if url
@@ -13,6 +10,5 @@ if url
   Sidekiq.configure_client do |config|
     config.redis = { url: url, ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }, network_timeout: 5 }
   end
-  $redis = Redis.new(url: url)
+
 end
-# rubocop:enable Style/GlobalVars
